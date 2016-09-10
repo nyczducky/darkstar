abed78aea334d4ba65e0ee73f57d027a42143493
nyczducky <sekigah@gmail.com>
2016-09-10 15:12:39 -0400

    Revert "Update from DSP - 9.10.16"

--- src/map/ai/states/magic_state.cpp
+++ src/map/ai/states/magic_state.cpp
@@ -290,8 +290,6 @@ void CMagicState::ApplyEnmity(CBattleEntity* PTarget, int ce, int ve)
                 ((CMobEntity*)PTarget)->updatemask |= UPDATE_STATUS;
             }
             ((CMobEntity*)PTarget)->PEnmityContainer->UpdateEnmity(m_PEntity, ce, ve);
-            if (PTarget && ((CMobEntity*)PTarget)->m_HiPCLvl < m_PEntity->GetMLevel())
-                ((CMobEntity*)PTarget)->m_HiPCLvl = m_PEntity->GetMLevel();
             enmityApplied = true;
         }
     }
